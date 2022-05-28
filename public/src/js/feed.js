89a667dddd5c45e7f66d555c1c23b9fa30443a8e
J. Freiheit <freiheit@htw-berlin.de>
2022-05-28 19:34:10 +0200

    Standort und Karte fertig

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -14,6 +14,93 @@ let file = null;
 let titleValue = '';
 let locationValue = '';
 let imageURI = '';
+let locationButton = document.querySelector('#location-btn');
+let locationLoader = document.querySelector('#location-loader');
+let fetchedLocation;
+let mapDiv = document.querySelector('.map');
+
+locationButton.addEventListener('click', event => {
+    if(!('geolocation' in navigator)) {
+        return;
+    }
+
+    locationButton.style.display = 'none';
+    locationLoader.style.display = 'block';
+
+    navigator.geolocation.getCurrentPosition( position => {
+        locationButton.style.display = 'inline';
+        locationLoader.style.display = 'none';
+        fetchedLocation = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+        console.log('current position: ', fetchedLocation);
+
+        let nominatimURL = 'https://nominatim.openstreetmap.org/reverse'; 
+        nominatimURL += '?format=jsonv2';   // format=[xml|json|jsonv2|geojson|geocodejson]
+        nominatimURL += '&lat=' + fetchedLocation.latitude;
+        nominatimURL += '&lon=' + fetchedLocation.longitude;
+
+        fetch(nominatimURL)
+            .then((res) => {
+                console.log('nominatim res ...', res);
+                return res.json();
+            })
+            .then((data) => {
+                console.log('nominatim res.json() ...', data);
+                locationInput.value = data.display_name;
+                return data;
+            })
+            .then( d => {
+                locationButton.style.display = 'none';
+                locationLoader.style.display = 'none';
+                mapDiv.style.display = 'block';
+
+                const map = new ol.Map({
+                    target: 'map',
+                    layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                    ],
+                    view: new ol.View({
+                    center: ol.proj.fromLonLat([fetchedLocation.longitude, fetchedLocation.latitude]),
+                    zoom: 12
+                    })
+                });
+
+                const layer = new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        features: [
+                            new ol.Feature({
+                                geometry: new ol.geom.Point(ol.proj.fromLonLat([fetchedLocation.longitude, fetchedLocation.latitude]))
+                            })
+                        ]
+                    })
+                });
+ 
+                map.addLayer(layer);
+
+                console.log('map', map)
+            })
+            .catch( (err) => {
+                console.error('err', err)
+                locationInput.value = 'In Berlin';
+            });
+
+        document.querySelector('#manual-location').classList.add('is-focused');
+
+    }, err => {
+        console.log(err);
+        locationButton.style.display = 'inline';
+        locationLoader.style.display = 'none';
+        alert('Couldn\'t fetch location, please enter manually!');
+        fetchedLocation = null;
+    }, { timeout: 5000});
+});
+
+function initializeLocation() {
+    if(!('geolocation' in navigator)) {
+        locationButton.style.display = 'none';
+    }
+}
 
 function initializeMedia() {
     if(!('mediaDevices' in navigator)) {
@@ -45,15 +132,25 @@ function initializeMedia() {
 }
 
 function openCreatePostModal() {
-    createPostArea.style.transform = 'translateY(0)';
+    setTimeout( () => {
+        createPostArea.style.transform = 'translateY(0)';
+    }, 1);
     initializeMedia();
+    initializeLocation();
 }
 
 function closeCreatePostModal() {
-    createPostArea.style.transform = 'translateY(100vH)';
     imagePickerArea.style.display = 'none';
     videoPlayer.style.display = 'none';
     canvasElement.style.display = 'none';
+    locationButton.style.display = 'inline';
+	locationLoader.style.display = 'none';
+    if(videoPlayer.srcObject) {
+        videoPlayer.srcObject.getVideoTracks().forEach( track => track.stop());
+    }
+    setTimeout( () => {
+        createPostArea.style.transform = 'translateY(100vH)';
+    }, 1);
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
